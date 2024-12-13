5aad0b5ba485ab8c01b7c3fe7da5818eabad3285
sidz111 <sssurwade2212@gmail.com>
2024-12-13 16:10:35 +0530

    update some functions

--- src/main/java/com/eagle/EmployeesEagleApplication.java
+++ src/main/java/com/eagle/EmployeesEagleApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeesEagleApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesEagleApplication.class, args);
 	}
